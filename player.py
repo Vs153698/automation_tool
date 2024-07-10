7f15bf55fa05d15c033c72ee54a8ed14e872fdf2
Vaibhav <vs153698@gmail.com>
2024-07-10 12:49:25 +0530

    automation tool new changes

--- player.py
+++ player.py
@@ -2,13 +2,24 @@ import time
 import pyautogui
 
 def play_actions(actions, speed=1.0):
+    if not actions:
+        return
+
     start_time = actions[0]['time']
+    last_time = start_time
+
     for action in actions:
-        time.sleep((action['time'] - start_time) / speed)
-        start_time = action['time']
+        current_time = action['time']
+        sleep_time = (current_time - last_time) / speed
+
+        # Only sleep if the time difference is significant
+        if sleep_time > 0.01:  # You can adjust this threshold
+            time.sleep(sleep_time)
+
+        last_time = current_time
 
         if action['type'] == 'move':
-            pyautogui.moveTo(action['position'][0], action['position'][1])
+            pyautogui.moveTo(action['position'][0], action['position'][1], duration=0)
         elif action['type'] == 'click':
             pyautogui.click(x=action['position'][0], y=action['position'][1], button=action['button'].split('.')[-1])
         elif action['type'] == 'keyboard':
@@ -16,3 +27,4 @@ def play_actions(actions, speed=1.0):
                 pyautogui.press(action['key'])
             elif action['event_type'] == 'release':
                 pyautogui.keyUp(action['key'])
+
